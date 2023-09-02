8098de7c2131e6a209a68a089e0bd9f02eaa7457
adolfojambrina2023 <143880055+adolfojambrina2023@users.noreply.github.com>
2023-09-02 18:47:24 +0200

    Updated 18:47

--- plot4.R
+++ plot4.R
@@ -16,15 +16,20 @@ cons_sub<-subset(consumption, Date>=as.POSIXlt("2007-02-01")&Date<as.POSIXlt("20
 Sys.setlocale("LC_TIME","English")
 
 #Represent the plot
+par(mfrow=c(2,2),cex=0.5)
 with(cons_sub,{
+  plot(Date,Global_active_power, type="l",xlab="", ylab="Global Active Power")
+  plot(Date, Voltage, type="l",xlab="datetime",ylab="Voltage")
   plot(Date,Sub_metering_1, type="l",xlab="", ylab="Energy sub metering", main="", col="black", cex=0.25)
   lines(Date, Sub_metering_2, type="l", col="red")
   lines(Date, Sub_metering_3,type="l", col="blue")
+  legend("topright",lty=1, cex=0.5, legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), col=c("black","red","blue"))
+  plot(Date,Global_reactive_power, type="l", xlab="datetime", ylab="Global_reactive_power")
   }
 )
-#Set legend
-legend("topright",lty=1, cex=0.5, legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), col=c("black","red","blue"))
+
+
 #Save the histogram into a png file
-dev.copy(png, file="plot3.png", width=480, height=480)
+dev.copy(png, file="plot4.png", width=480, height=480)
 dev.off()
 
